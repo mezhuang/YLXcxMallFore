d3e2977492326f834964a6316a648def2f3fe8b0
mezhuang <306637137@qq.com>
2018-06-23 03:35:47 +0800

    增加个人中心前端byYsx

--- pages/myhome/myhome.js
+++ pages/myhome/myhome.js
@@ -14,8 +14,36 @@ Page({
     isReferManger:false,
     isReferDicrector: false,
     isGeneral: true,
-    groupCode:null
+    groupCode:null,
     
+
+    // orderItems
+    orderItems: [
+      {
+        typeId: 0,
+        name: '待付款',
+        url: 'bill',
+        imageurl: '../../images/icon/myhome/personal_pay.png',
+      },
+      {
+        typeId: 1,
+        name: '待发货',
+        url: 'bill',
+        imageurl: '../../images/icon/myhome/personal_shipped.png',
+      },
+      {
+        typeId: 2,
+        name: '待收货',
+        url: 'bill',
+        imageurl: '../../images/icon/myhome/personal_receipt.png'
+      },
+      {
+        typeId: 3,
+        name: '待评价',
+        url: 'bill',
+        imageurl: '../../images/icon/myhome/personal_comment.png'
+      }
+    ],
     
   },
   /**
@@ -171,9 +199,26 @@ Page({
     wx.navigateTo({
       url: '../sysManager/sysManager'
     })
+  },
+
+
+
+  //事件处理函数
+  toOrder: function () {
+    wx.navigateTo({
+      url: '../order/order'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
   }
-
-
-
 
 })
