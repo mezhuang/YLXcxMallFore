e2bb11a53fec4202a6a984b70d7576c7be5d5305
mezhuang <306637137@qq.com>
2018-07-05 19:26:48 +0800

    完善详情页及购买功能byYsx

--- pages/goodsManager/goodsDetail/goodsDetail.js
+++ pages/goodsManager/goodsDetail/goodsDetail.js
@@ -1,5 +1,11 @@
+const app = getApp()
+
 Page({
   data: {
+    swerposition:"07001",
+    detailposition:"07002",
+    curFormatPriceId:null,//被选中的规格编码
+    curDiplayPrice:null,
     isLike: true,
     // banner
     imgUrls: [
@@ -15,14 +21,15 @@ Page({
     duration: 1000, //  滑动动画时长1s
 
     // 商品详情介绍
-    detailImg: [
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_1.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_2.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_3.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_4.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_5.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_6.jpg",
-    ],
+    goodsDetail:null,
+    // detailImg: [
+    //   "http://7xnmrr.com1.z0.glb.clouddn.com/detail_1.jpg",
+    //   "http://7xnmrr.com1.z0.glb.clouddn.com/detail_2.jpg",
+    //   "http://7xnmrr.com1.z0.glb.clouddn.com/detail_3.jpg",
+    //   "http://7xnmrr.com1.z0.glb.clouddn.com/detail_4.jpg",
+    //   "http://7xnmrr.com1.z0.glb.clouddn.com/detail_5.jpg",
+    //   "http://7xnmrr.com1.z0.glb.clouddn.com/detail_6.jpg",
+    // ],
 
     //购买数量
     // input默认是1  
@@ -48,15 +55,38 @@ Page({
   // 跳到购物车
   toCar() {
     wx.switchTab({
-      url: '/pages/cart/cart'
+      url: '/pages/payCart/payCart'
     })
   },
-  // 立即购买
-  immeBuy() {
-    wx.showToast({
-      title: '购买成功',
-      icon: 'success',
-      duration: 2000
+  
+  // immeBuy() {
+
+  //   wx.showToast({
+  //     title: '购买成功',
+  //     icon: 'success',
+  //     duration: 2000
+  //   });
+  // },
+// 立即购买
+  immeBuy: function (event) {
+    // console.log("e.currentTarget.dataset.totalFee:");
+    // console.log(event.target.dataset.totalfee);
+    // console.log("e.currentTarget.dataset.goodscode:");
+    // console.log(event.target.dataset.goodscode);
+    var that =this;
+    wx.setStorage({
+      key: "totalFee",
+      // data: event.target.dataset.totalfee
+      data: curDiplayPrice
+    });
+
+    wx.setStorage({
+      key: "goodsCode",
+      data: that.data.goods_id
+    });
+    console.log("tesfftss");
+    wx.navigateTo({
+      url: '../pay/pay'
     });
   },
 
@@ -98,6 +128,89 @@ Page({
     this.setData({
       num: num
     });
-  }  
+  },
+/**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //获取商品编码
+
+    var goodsId = options.id;
+
+
+
+    var that = this;
+    wx.login({
+      success: function (res) {
+
+
+      }
+    })
+
+    wx.request({
+      method: 'GET',
+      // url: 'www.yuanlianjj.com?token=' + tokend, //接口地址
+      url: app.globalData.serviceIp + 'getGoodsRecordDetail.do', //接口地址
+      data: {
+        'openId': getApp().globalData.openId,
+        'startIndex': 0,
+        'indexSize': 5,
+        "id": goodsId
+      },
+      header: { 'content-type': 'application/json' },
+      success: function (res) {
+        console.log('success-res' + ':' + res.data)
+        that.setData({
+          goodsDetail: res.data,
+          curDiplayPrice: res.data[0].goodsFormatList[0].curr_price
+
+        });
+
+        //跳转至报备客户列表
+        //   wx.navigateTo({
+        //     url: "../reportList/reportList"
+        //   })
+
+      },
+      fail: function (res) {
+        console.log('fail-res' + ':' + res.data)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.onLoad();
+  },
+  /**选择花色 */
+  chooseGoodsFormat: function (data) {
+    var that = this;
+    var formatpriceid = data.currentTarget.dataset.formatpriceid;
+    var currPrice = data.currentTarget.dataset.currprice;
+
+    that.setData({//把选中值，放入判断值中
+      formatSelectId: formatpriceid,
+      curDiplayPrice: currPrice
+    })
+    // var str = flower_id + ',' + flower_name;
+    // var chooseFlowers = that.data.chooseFlowers;
+    // chooseFlowers = [];
+    // chooseFlowers.push(str);
+    // that.setData({
+    //   chooseFlowers: chooseFlowers,
+    //   flowerImgSelect: data.currentTarget.dataset.img
+    // })
+
+
+  }
 
 })
